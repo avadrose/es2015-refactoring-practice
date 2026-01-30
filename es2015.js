dbd9cb871a0bb3ac3fc0ac0e3d847e1f68890fc3
avadrose <avadrose@yahoo.com>
2026-01-30 02:17:23 -0500

    arrow functions exercise

--- es2015.js
+++ es2015.js
@@ -2,3 +2,6 @@ const PI = 3.14;
 const nums = [1, 2, 3];
 
 const double = arr => arr.map(num => num * 2);
+
+const squareAndFindEvens = numbers =>
+    numbers.map(num => num ** 2).filter(square => square % 2 === 0);
--- es5.js
+++ es5.js
@@ -6,3 +6,13 @@ function double(arr) {
     return num * 2;
   });
 }
+
+function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}
